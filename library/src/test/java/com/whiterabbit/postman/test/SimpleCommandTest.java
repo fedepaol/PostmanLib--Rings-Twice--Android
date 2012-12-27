cd73f8da3816f14522d1286345c9d624308fde15
fedepaol <fedepaol@gmail.com>
2012-12-27 23:41:18 +0100

    Prepared robolectric tests

---
+++ library/src/test/java/com/whiterabbit/postman/test/SimpleCommandTest.java
@@ -0,0 +1,26 @@
+package com.whiterabbit.postman.test;
+
+import com.xtremelabs.robolectric.RobolectricTestRunner;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(RobolectricTestRunner.class)
+public class SimpleCommandTest {
+
+
+	@Before
+    public void setUp() throws Exception {
+
+
+    }
+
+
+
+	@Test
+    public void testTry(){
+
+	}
+
+}
+
