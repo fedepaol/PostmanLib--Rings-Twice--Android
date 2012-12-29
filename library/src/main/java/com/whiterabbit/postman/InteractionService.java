23f1b4dd2b8a396a80c29e8e4ab64c4c89d8a60f
fedepaol <fedepaol@gmail.com>
2012-12-29 11:54:57 +0100

    Unused code cleanup

--- library/src/main/java/com/whiterabbit/postman/InteractionService.java
+++ library/src/main/java/com/whiterabbit/postman/InteractionService.java
@@ -6,7 +6,6 @@ import android.app.IntentService;
 import android.content.Intent;
 import android.util.Log;
 import com.whiterabbit.postman.commands.ServerCommand;
-import com.whiterabbit.postman.com.whiterabbit.postman.exceptions.UnknownCommandException;
 import com.whiterabbit.postman.utils.Constants;
 
 
@@ -24,33 +23,14 @@ public class InteractionService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
-    	try{
-            String messageType = ServerCommand.getTypeFromIntent(intent);
-            //try {
-                    ServerCommand c = (ServerCommand) intent.getParcelableExtra(Constants.PAYLOAD);
+        ServerCommand c = (ServerCommand) intent.getParcelableExtra(Constants.PAYLOAD);
+        if(c != null){
+            c.execute(this);
+        }else{
+            Log.e(Constants.LOG_TAG, "Unable to convert message");
+            ServerCommand.notifyUnrecoverableError(intent, getString(R.string.unable_to_convert), this);
+        }
 
-
-                            //Class.forName(messageType).getConstructor(Intent.class).newInstance(intent);
-                    c.execute(this);
-
-            if (null == c){
-                throw new UnknownCommandException();
-            }
-            /*
-            } catch (ClassNotFoundException e) {
-
-            } catch (InstantiationException e) {
-            } catch (IllegalAccessException e) {
-            } catch (InvocationTargetException e) {
-            } catch (NoSuchMethodException e) {
-                throw new UnknownCommandException();
-            }
-            */
-
-    	}catch (UnknownCommandException e){
-    		Log.e(Constants.LOG_TAG, "Unable to convert message");
-    		ServerCommand.notifyUnrecoverableError(intent, getString(R.string.unable_to_convert), this);
-    	}
     }
 
 }
