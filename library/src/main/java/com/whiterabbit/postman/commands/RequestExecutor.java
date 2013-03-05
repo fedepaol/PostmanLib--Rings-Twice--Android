b25774fcde4172c653fcc1906b117210aa4601fb
fedepaol <fedepaol@gmail.com>
2013-03-05 23:32:25 +0100

    Added request executor

---
+++ library/src/main/java/com/whiterabbit/postman/commands/RequestExecutor.java
@@ -0,0 +1,18 @@
+package com.whiterabbit.postman.commands;
+
+import android.content.Context;
+import com.whiterabbit.postman.exceptions.PostmanException;
+
+/**
+ * Utility interface to be implemented in order to execute requests
+ */
+public interface RequestExecutor {
+    /**
+     * Tries to execute the given request
+     * @param s the strategy to be executed
+     * @param c
+     * @throws PostmanException
+     */
+    void executeRequest(RestServerRequest s, Context c) throws PostmanException;
+
+}
